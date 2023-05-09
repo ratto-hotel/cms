9881926a252d77912268701811e5c83ecc9cc9f4
Jean Nunes <jeanrnunes@gmail.com>
2023-05-09 11:07:49 -0300

    initial commit

--- lang/br/passwords.php
+++ lang/br/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Sua senha foi redefinida!',
+    'sent' => 'Enviamos um email com o link de redefinição de senha!',
+    'throttled' => 'Por favor, aguarde antes de tentar novamente.',
+    'token' => 'O código de redefinição de senha é inválido.',
+    'user' => "Não encontramos nenhum usuário com esse e-mail.",
 
 ];
---
+++ update.bat
@@ -0,0 +1,9 @@
+git pull origin main
+
+composer install
+
+php artisan migrate
+
+npm install
+
+npm run build:atom
